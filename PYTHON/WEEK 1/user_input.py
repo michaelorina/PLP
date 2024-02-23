117f7dc06e05a824f0f91b260718f9ab6e272b52
michaelorina <orinamichael4@gmail.com>
2024-02-23 14:08:25 +0300

    Python Week 1

---
+++ PYTHON/WEEK 1/user_input.py
@@ -0,0 +1,14 @@
+# user_input.py
+
+# Ask user for their name
+name = input("What is your name? ")
+
+# Ask user for their age
+age = input("How old are you? ")
+
+# Ask user for their location
+location = input("Where are you located? ")
+
+# Print out personalized message
+print("Hello {}, you are {} years old and live in {}.".format(name, age, location))
+
